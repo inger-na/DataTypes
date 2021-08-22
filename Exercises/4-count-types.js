066c36ab4e0f50e7b5d07d16a17b2716862853ac
inger-na <inger_na@mail.ru>
2021-08-22 10:56:23 +0200

    All Exercises solved

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const obj = {};
+  for (const val of array) {
+    const type = typeof val;
+    (!obj[type]) ?  Object.assign(obj, { [type]: 1 }) :  obj[type] += 1;
+  }
+  return obj;
+};
 
 module.exports = { countTypesInArray };
